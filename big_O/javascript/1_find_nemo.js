d7d64c33ec465a3850f5aeb20ea58615a90a48da
CristoferNava <cj.carmonanava@ugto.mx>
2019-12-05 14:24:37 -0600

    Performace measure

---
+++ big_O/javascript/1_find_nemo.js
@@ -0,0 +1,17 @@
+const name = ['nemo'];
+const names = ['cristofer', 'oliver', 'josue', 'monse', 'gerardo', 'pollo', 
+                'gallo'];
+const large = new Array(100000).fill('pollo')
+
+function findNemo(array) {
+    let t0 = performance.now();
+    for (let i = 0; i <= array.length - 1; i++) {
+        if (array[i] === 'nemo') {
+            console.log('We found Nemo!');
+        }
+    }
+    let t1 = performance.now();
+    console.log(`${t1 - t0} milliseconds`);
+}
+
+findNemo(large);
