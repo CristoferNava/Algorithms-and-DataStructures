67891847e0fd03a79ab0330759e680cdf10ec15d
CristoferNava <cj.carmonanava@ugto.mx>
2019-12-14 13:15:55 -0600

    Nonincreasing insertion-sort was added

---
+++ MIT/nonincreasing-insertion-sort.js
@@ -0,0 +1,19 @@
+// Nonincreasing insertion-sort
+
+function insertionSort(array) {
+  let key;
+  for (let j = 0; j < array.length; j++) {
+    key = array[j];
+    i = j - 1;
+    while (i >= 0 && key > array[i]) {
+      array[i + 1] = array[i];
+      i = i - 1;
+    }
+    array[i + 1] = key;
+  }
+  return array;
+}
+
+let numbers = [];
+insertionSort(numbers);
+console.log(insertionSort(numbers));
