2b27700f7384927f7598c09a534e1ccf60c13981
CristoferNava <cj.carmonanava@ugto.mx>
2019-12-28 21:41:20 -0600

    Reverse string was added

---
+++ interview-questions/2-reverse-string.js
@@ -0,0 +1,9 @@
+function reverse(string) {
+  let elements = [];
+  for (let i = string.length - 1; i >= 0; i--) {
+    elements.push(string[i]);
+  }
+  elements = (String(elements)).replace(/,/g, '');
+  
+  return elements;
+}
