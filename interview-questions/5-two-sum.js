988f9cdd44a2df53550cd9fe120c9347f7a0e013
CristoferNava <cj.carmonanava@ugto.mx>
2020-01-21 13:41:42 -0600

    Made easy was created

--- interview-questions/5-two-sum.js
+++ interview-questions/5-two-sum.js
@@ -5,7 +5,7 @@
 // Given nums = [2, 7, 11, 15], target = 9, return [0, 1]
 
 // First solution (naive one)
-function twoSum(arr, target) {
+function naiveTwoSum(arr, target) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] + arr[j] === target) {
@@ -15,6 +15,10 @@ function twoSum(arr, target) {
   }
 }
 
+const map = {};
 const array = [1, 3, 11, 2, 5, 8, 12];
 const target = 4;
-console.log(twoSum(array, target));
+
+array.forEach((element, index) => {
+  map[index] = element;
+});
