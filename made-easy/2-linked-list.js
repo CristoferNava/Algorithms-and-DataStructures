ef285bd8add3569781a7ee576ca0ec841f0188f8
CristoferNava <cj.carmonanava@ugto.mx>
2020-01-22 11:07:51 -0600

    Search was added to the linked list

--- made-easy/2-linked-list.js
+++ made-easy/2-linked-list.js
@@ -1,23 +1,62 @@
-class ListNode {
+// Inserts (append, prepend, random-location)
+
+class Node {
   constructor(data) {
     this.data = data;
+    this.next = null;
   }
 }
 
-const test = {
-  head: {
-    data: 4,
-    next: {
-      data: 15,
-      next: {
-        data: 40,
-        next: {
-          data: 7,
-          next: null,
-        }
-      }
-    }, 
+class LinkedList {
+  constructor(data) {
+    this.head = new Node(data);
+    this.tail = this.head;
+    this.length = 1;
   }
-};
 
-console.log(test.head.next.next.next.next.data);
+  append(data) {
+    const node = new Node(data);
+    this.tail.next = node;
+    this.tail = node;
+    this.length++;
+  }
+
+  prepend(data) {
+    const node = new Node(data);
+    node.next = this.head,
+    this.head = node;
+    this.length++;
+  }
+
+  insert(index) {
+
+  }
+
+  search(index) {
+    let currentNode = this.head;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+
+    return currentNode;
+  }
+
+  values() {
+    let currentNode = this.head;
+    const values = [];
+    while (currentNode !== null) {
+      values.push(currentNode.data);
+      currentNode = currentNode.next;
+    }
+
+    return values;
+  }
+}
+
+// 5, 7, 4, 1, 3
+let linkedList = new LinkedList(5);
+linkedList.append(7);
+linkedList.prepend(2)
+console.log(linkedList.values());
+console.log(linkedList.length);
+console.log(linkedList.search(3).data);
