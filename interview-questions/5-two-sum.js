b7d7dd4ce984be26364a00bc4f2bf0001c6805bd
CristoferNava <cj.carmonanava@ugto.mx>
2020-01-30 18:11:27 -0600

    Two sum was added

--- interview-questions/5-two-sum.js
+++ interview-questions/5-two-sum.js
@@ -4,8 +4,11 @@
 // not use the same element twice.
 // Given nums = [2, 7, 11, 15], target = 9, return [0, 1]
 
+let numbers = [2, 7, 11, 15];
+let target = 9;
+
 // First solution (naive one)
-function naiveTwoSum(arr, target) {
+function twoSumFirst(arr, target) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] + arr[j] === target) {
@@ -13,12 +16,18 @@ function naiveTwoSum(arr, target) {
       }
     }
   }
-}
+} // Time Complexity: O(n^2) and Space Complexity: O(1)
 
-const map = {};
-const array = [1, 3, 11, 2, 5, 8, 12];
-const target = 4;
+function twoSumHash(arr, target) {
+  let map = {};
+  for (let i = 0; i < arr.length; i++) {
+    map[arr[i]] = i;
+  }
 
-array.forEach((element, index) => {
-  map[index] = element;
-});
+  for (let i = 0; i < arr.length; i++) {
+    let complement = target - arr[i];
+    if (map.hasOwnProperty(complement) && map[complement] !== i) {
+      return [i, map[complement]];
+    }
+  }
+} // Time Complexity: O(n) and Space complexity: O(n)
