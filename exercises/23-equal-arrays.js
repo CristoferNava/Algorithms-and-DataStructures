3390dea8976720cd5128935aa4657d33cb8be3e6
CristoferNava <cj.carmonanava@ugto.mx>
2020-02-17 07:22:54 -0600

    Equals arrays was added

--- exercises/23-equal-arrays.js
+++ exercises/23-equal-arrays.js
@@ -1,18 +1,10 @@
-let equals = true;
-
-function equalArrays(arr1, arr2, pos) {
-  if (pos !== arr1.length) {
-    console.log(`${arr1[pos]} ${arr2[pos]}`);
+function equalsArrays1(arr1, arr2, pos, equals) {
+  if (pos < arr1.length) {
     if (arr1[pos] !== arr2[pos]) {
       equals = false;
     }
-    equalArrays(arr1, arr2, pos + 1);
-  } else {
-    console.log(equals);
+    return equalsArrays1(arr1, arr2, pos + 1, equals);
   }
-}
 
-let array1 = [2, 32, 12, 1, 0];
-let array2 = [24, 12, 11, 4, 5];
-let array3 = [2, 32, 12, 1, 0];
-equalArrays(array1, array3, 0);
+  return equals;
+}
