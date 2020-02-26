cc1e8f5ea4621a7b40b4911387e2e8f56256ef5c
CristoferNava <cj.carmonanava@ugto.mx>
2020-02-25 22:12:24 -0600

    First implementation of a stack using linked-list was added

--- data-structures/stacks&queues/2-stack-implementation.js
+++ data-structures/stacks&queues/2-stack-implementation.js
@@ -11,10 +11,33 @@ class Stack {
     this.bottom = null;
     this.length = 0;
   }
+
   peek() {
+    return this.top.value;
   }
-  push() {
+
+  push(value) {
+    this.bottom = this.top;
+    const node = new Node(value);
+    this.top = node;
+    this.top.next = this.bottom;
+    this.length++;
   }
+
   pop() {
+    delete this.top;
+    this.top = this.bottom;
+    this.bottom = this.top.next;
+    this.length--;
   }
 }
+
+let myStack = new Stack();
+myStack.push("Js");
+myStack.push("Python");
+myStack.push("Java");
+console.log(myStack.peek());
+myStack.pop();
+console.log(myStack.peek());
+myStack.pop();
+console.log(myStack.peek());
