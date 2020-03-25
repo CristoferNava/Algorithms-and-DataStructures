8acf5d2e6e1e86dc9a4dda3f92ac3c4a3852636b
CristoferNava <cj.carmonanava@ugto.mx>
2020-03-24 22:55:44 -0600

    first version of add was added (testing version)

--- data-structures/trees/1-binary-search-tree.js
+++ data-structures/trees/1-binary-search-tree.js
@@ -16,17 +16,76 @@ class BinarySearchTree {
     // if it is the first node then it will be the root
     if (this.root === null) {
       this.root = newNode;
-      return;
+      return true;
     }
     
+    // Inserting in another place
+    let currentNode = this.root;
+    while (true) {
+      // to the left
+      if (newNode.data < currentNode.data) {
+        // if there's not node to the leff
+        if (currentNode.left === null) {
+          currentNode.left = newNode;
+          return true;
+        }
+        // if there's a node to the left
+        currentNode = currentNode.left;
+        continue;
+      }
+
+      // to the left
+      if (newNode.data > currentNode.data) {
+        // if there's not a node to the left
+        if (currentNode.right === null) {
+          currentNode.right = newNode;
+          return true;;
+        }
+
+        // if there's a node to the right
+        currentNode = currentNode.right;
+      }
+    }
+
+    // if the node to insert already exists
+    return false;
   }
 
   search(data) {
+    let currentNode = this.root;
+    if (data === this.root.data) {
+      return true;
+    }
 
+    while (true) {
+      // to the left
+      if (data < currentNode.data) {
+        currentNode = currentNode.left;
+        if (currentNode.data === data) return true;
+        continue;
+      }
+
+      // to the right
+      if (data > currentNode.data) {
+        currentNode = currentNode.right;
+        if (currentNode.data === data) return true;
+      }
+
+      return false;
+    }
   }
 }
 
 //       9 
 //   4      20
-// 1   6  5   170
+// 1   6  18   170
 const myTree = new BinarySearchTree();
+myTree.insert(9);
+myTree.insert(4);
+myTree.insert(20);
+myTree.insert(1);
+myTree.insert(6);
+myTree.insert(18);
+myTree.insert(170);
+
+console.log(myTree.root.right.right.data);
