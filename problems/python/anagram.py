73ec40e5aaccdb01bcb9eadb12faa2a7cd23ab1b
CristoferNava <cj.carmonanava@ugto.mx>
2020-08-25 20:09:50 -0500

    Description of the anagram problem was added

---
+++ problems/python/anagram.py
@@ -0,0 +1,14 @@
+# Given two strings, check to see if they are anagrams. 
+# An anagram is when the two strings can be written using the exact same 
+# letters (so you can just rearrange the letters to get a different phrase or 
+# word).
+# Note: Ignore spaces and capitalization. So "d go" is an anagram of "God" and 
+# "dog" and "o d g"
+
+def is_anagram(str1: str, str2: str) -> bool:
+    pass
+
+# Test cases
+print(is_anagram('dog','god')) # True
+print(is_anagram('clint eastwood','old west action')) # True
+print(is_anagram('aa', 'bb')) # False
