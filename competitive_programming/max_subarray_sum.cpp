9b8a5581e494ab1025f65165a33e21937f1aaa00
CristoferNava <cj.carmonanava@ugto.mx>
2020-09-20 12:07:44 -0500

    O(n^2) was added

--- competitive_programming/max_subarray_sum.cpp
+++ competitive_programming/max_subarray_sum.cpp
@@ -4,17 +4,34 @@
 
 using namespace std;
 
-int maxSubarraySum(vector<int>);
+int maxSubarraySum2(vector<int>);
+int maxSubarraySum3(vector<int>);
 
 int main() {
     vector<int> numbers = {-1, 2, 4, -3, 5, 2, -5, 2};
 
-    cout << maxSubarraySum(numbers) << "\n";  
+    cout << maxSubarraySum2(numbers) << "\n";  
 
     return 0;
 }
 
-int maxSubarraySum(vector<int> arr) {
+int maxSubarraySum2(vector<int> arr) {
+    // time complexity: O(n^2)
+    // space complexity: O(1)
+    int best = 0; // max sum value
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0; // calculate the sum while is traversing the array
+        for (int j = i; j < n; j++) {
+            sum += arr[j];
+            best = max(best, sum);
+        }
+    }
+    return best;
+}
+
+int maxSubarraySum3(vector<int> arr) {
     // time complexity: O(n^3)
     // space complexity: O(1)
     int best = 0; // max sum calculated
