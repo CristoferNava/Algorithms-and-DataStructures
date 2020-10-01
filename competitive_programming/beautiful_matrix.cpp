eea4cd2b4aa96d45a39fb4f155a8aaf470c4aead
CristoferNava <cj.carmonanava@ugto.mx>
2020-09-30 22:45:58 -0500

    beautiful matrix

---
+++ competitive_programming/beautiful_matrix.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int matrix[5][5];
+    int p1, p2;
+    int b1 = 2, b2 = 2;
+    int steps = 0;
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> matrix[i][j];
+            if (matrix[i][j] == 1) p1 = i, p2 = j; 
+        }
+    }
+
+    // first move p1 to the correct position
+    // if p1 == b1 no hagas nada
+    // if p1 < b1 incrementa p1 hasta llegar a b1 b1 - p1 
+    // if p1 > b1 decrementa b1 hata llegar b1
+
+    if (p1 < b1) steps += b1 - p1;
+    else if (p1 > b1) steps += p1 - b1;
+
+    if (p2 < b2) steps += b2 - p2;
+    else if (p2 > b2) steps += p2 - b2;
+
+    cout << steps << endl;
+}
