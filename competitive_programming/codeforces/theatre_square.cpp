42164487e4083c26b01ae37ef4cf48bf5b1c0a1a
CristoferNava <cj.carmonanava@ugto.mx>
2020-10-18 17:19:13 -0500

    Theatre Square

---
+++ competitive_programming/codeforces/theatre_square.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long double n, m;
+    long double a; 
+    cin >> n >> m >> a;
+
+    int t = 0;
+
+    n /= a;
+    if ((long long int)n != n) n++;
+    n = (long long int)n;
+    
+
+    m /= a;
+    if ((long long int)m != m) m++;
+    m = (long long int)m;
+
+    cout << (long long int)(n * m) << "\n";
+}
