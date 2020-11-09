484cb8854f3cbedf3e167849977413cf1e0287d4
CristoferNava <cj.carmonanava@ugto.mx>
2020-11-09 16:51:51 -0600

    string was added to primer/3

--- competitive_programming/learning_c++/primer/3/string.cpp
+++ competitive_programming/learning_c++/primer/3/string.cpp
@@ -9,4 +9,6 @@ int main() {
     string s2 = s1; // s2 is a copy of s1
     string s3 = "hiya"; // s3 is a copy of the string literal
     string s4(5, 'c'); // s4 is ccccc 
+    string s5 = "hiya"; // copy initialization, the right side copies to the object of the left side
+    string s6("hiya"); // direct initialization
 }
