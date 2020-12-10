1139a9e6a3224027f653fad8179ce7aa493f9cda
CristoferNava <cj.carmonanava@ugto.mx>
2020-12-10 11:54:55 -0600

    lists.py was added

---
+++ competitive_programming/aurora/comp.py
@@ -0,0 +1,12 @@
+x = 2
+y = 2
+# and, or, not 
+# False, True
+
+# 1 < x < 10
+if 1 < x < 10:
+    print('está entre el 1 y el 10')
+# elif x < y:
+#     print(x)
+# else:
+#     print(x)
---
+++ competitive_programming/aurora/intro.py
@@ -0,0 +1,14 @@
+# built in
+i = 2 # int
+f = 2.0 # float
+b = False # bool
+s = 'h' # str
+
+x = 2
+
+
+
+if x > 3:
+    print('entró')
+else:
+    print('no entró')
