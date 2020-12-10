74829651c7b9e25930980b8fdec4d8922157eb14
CristoferNava <cj.carmonanava@ugto.mx>
2020-12-10 16:26:09 -0600

    strings was added

---
+++ competitive_programming/learning_python/others/strings.py
@@ -0,0 +1,10 @@
+s = 'hello'
+s = s.capitalize() # converts the first character to upper case
+s = s.lower()
+s = s.upper()
+n = s.count('H') # returns the number of times a specified value occurs in a string
+p = s.find('Ñ') # returns the position of the value if was found, if not it returns -1
+i = s.index('H')
+t = s.islower() # Returns True if all characters are in lower case
+t = s.isnumeric() # Returns True if all characters in the string are numeric
+print(i)
