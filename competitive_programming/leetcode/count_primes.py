0dbdfb63e8b485ad5aa3580e5bdf96ae9f7335dd
CristoferNava <cj.carmonanava@ugto.mx>
2020-12-30 14:10:17 -0600

    count_primes was added

--- competitive_programming/leetcode/count_primes.py
+++ competitive_programming/leetcode/count_primes.py
@@ -1,39 +1,18 @@
-from math import sqrt
-def countPrimes(n: int) -> int:
+def count_primes(n):
     if n in (0, 1):
         return 0
+    primes = 0
+    is_prime = [1 for i in range(n)]
+    is_prime[0] = is_prime[1] = 0
 
-    counter = 0
-    for i in range(2, n): 
-        prime = True
-        for j in range(2, int(sqrt(i)) + 1): 
-            if i % j == 0 and i != j:
-                # not is prime
-                prime = False
-                break
-                
-        if prime:
-            counter += 1
-    
-    return counter
-'''
-class Solution {
-    public int countPrimes(int n) {
-        int[] memoNoPrimos= new int[n+5];
-        int amount=0;
-        for(int i=2; i<n; i++){ 
-            //Preguntar si estoy marcado, NoPrimo
-            if(memoNoPrimos[i]==0){//Si soy primo
-                amount++;
-               for(int j=i; j<n; j+=i){ O(LogN)
-                    memoNoPrimos[j]=1;
-                } 
-            }
-        }
-        return amount;
-    }
-}
-'''
-result = countPrimes(10)
-print(result)
+    for i in range(n):
+        if is_prime[i]:
+            primes += 1
+            m = 2
+            while i*m < n:
+                is_prime[i*m] = 0
+                m += 1
 
+    return primes
+
+print(count_primes(10))
