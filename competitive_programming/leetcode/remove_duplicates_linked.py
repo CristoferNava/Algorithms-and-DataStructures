ab561bcaf9ac31feaaf32463f08c3162188c3b3f
CristoferNava <cj.carmonanava@ugto.mx>
2020-12-31 23:03:56 -0600

    ignore this

--- competitive_programming/leetcode/remove_duplicates_linked.py
+++
@@ -1,9 +0,0 @@
-def deleteDuplicates(self, head):
-    # Traverse the Linked List
-    node = head
-    while node.next:
-        if node.val == node.next.val:
-            node.next = node.next.next 
-        node = node.next  
-
-    return head
